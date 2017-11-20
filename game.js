cac142c38c62d4c1c21f9642ab87022396fef89f
Harut1999 <31660545+Harut1999@users.noreply.github.com>
2017-11-20 21:59:14 +0400

    Update game.js

--- game.js
+++ game.js
@@ -2,4 +2,6 @@ const canvas=document.getElementById('canvas')
 const c=canvas.getContext("2d")
 canvas.width= window.innerWidth;
 canvas.height=window.innerHeight;
+c.fillStyle="red"
+c.fillRect(canvas.width/4.5, canvas.height/12, canvas.width/4.5+500, canvas.height/12+600)
 
