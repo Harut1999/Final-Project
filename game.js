9ff3f77b5bddaf9fd41f88736d8451071a167d49
Violeta-iskandaryan <31819002+Violeta-iskandaryan@users.noreply.github.com>
2017-11-20 22:15:57 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,42 @@
 const canvas=document.getElementById('canvas')
-const c=canvas.getContext("2d")
+const ctx=canvas.getContext("2d")
 canvas.width= window.innerWidth;
 canvas.height=window.innerHeight;
-c.fillStyle="red"
-c.fillRect(canvas.width/4.5, canvas.height/12, canvas.width/4.5+500, canvas.height/12+600)
+const bg = {
+            x: 0,
+            y:0
 
+        }
+        const bg2 = {
+            x:0,
+            y:canvas.height
+
+        }
+ const bgImage = new Image();
+ bgImage.src = "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg";
+        
+ const bgImage2 = new Image();
+ bgImage2.src = "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__340.jpg";
+
+
+const draw = function () {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(bgImage, bg.x, bg.y, canvas.width, canvas.height);
+            ctx.drawImage(bgImage2,bg2.x, bg2.y, canvas.width, canvas.height);
+            
+        }
+const update = function () {
+
+            bg.y=bg.y-2;
+            bg2.y=bg2.y-2;
+           if (bg2.y<= 0) {
+               bg.y = 0;
+               bg2.y = canvas.height;
+           }
+        }
+ const loop = function () {
+            draw();
+            update();
+            requestAnimationFrame(loop);
+        }
+        loop();
